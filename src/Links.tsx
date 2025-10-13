3a7aea5af38a59c9370ee8f121024d1cef8b3a68
aldinpramudya <aldinarielpramudya@gmail.com>
2025-10-13 17:11:35 +0700

    feat :links changes

--- src/Links.tsx
+++ src/Links.tsx
@@ -11,8 +11,8 @@ export default function Links() {
                     rel="noopener noreferrer"
                     className="flex items-center gap-3 hover:text-[#FF6A00] transition-all duration-300"
                 >
-                    <span className="text-2xl">{social.icon}</span>
-                    <span className="text-2xl font-semibold">{social.name}</span>
+                    <span className="2xl:text-2xl text-xl">{social.icon}</span>
+                    <span className="2xl:text-2xl font-semibold">{social.name}</span>
                 </a>
             ))}
         </div>
