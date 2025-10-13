6e7f8db84aaad9f398836f9c0b312ca1907b3917
aldinpramudya <aldinarielpramudya@gmail.com>
2025-10-13 12:34:05 +0700

    feat : pages title, name, and description and profile picture

---
+++ constants/index.tsx
@@ -0,0 +1,23 @@
+export const socialLinks = [
+    {
+        name: "Instagram",
+        link: "https://www.instagram.com/aldin.pramudya"
+    },
+    {
+        name: "RIEL.Studio",
+        link: "https://www.instagram.com/riel.studio_/"
+    },
+    {
+        name: "Linkedin",
+        link: "https://www.linkedin.com/in/aldinarielpramudya/"
+    },
+    {
+        name : "Facebook",
+        link: "https://www.facebook.com/aldin.a.pramudya"
+    },
+    {
+        name: "X",
+        link: "https://x.com/rielelesia"
+    }
+];
+
--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,28 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <div className=''>
+    <div className='flex justify-center items-center min-h-screen'>
+      <div className='grid grid-cols-2 place-items-center mx-70'>
+        {/* Col 1 */}
+        <div>
+          <img src="/images/pic.jpg" alt="Profile Picture" className='w-[400px] rounded-full object-contain' />
+        </div>
+        {/* Col 2 */}
+        <div>
+          {/* Typography */}
+          <div className='leading-25'>
+            <h2 className='text-[35px] font-bold'>Hello !</h2>
+            <h1 className='text-[128px] font-bold tracking-tighter'>I'm Aldin</h1>
+            <p className='text-xl pt-10 tracking-tight'>Wassup ! I’m a web ethusiast based on Indonesia. <br />
+              Everyday i perfecting my craft on making websites to give you value and solution : &#10223;.
+            </p>
+          </div>
+          <div>
 
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
