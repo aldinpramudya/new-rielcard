9a92ab48dedaede32a68fee8effbd239fa22e8b3
aldinpramudya <aldinarielpramudya@gmail.com>
2025-10-16 00:07:56 +0700

    changing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // biar bisa diakses dari IPv4 & IPv6
+    port: 1337
+  }
 })
